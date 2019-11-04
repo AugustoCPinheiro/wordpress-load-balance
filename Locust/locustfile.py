baff0447d4fe0dbfd5a3f52952cc05fbe8ad33f0
Augusto <augustocepinheiro@gmail.com>
2019-11-04 16:39:07 -0300

    more results

--- Locust/locustfile.py
+++ Locust/locustfile.py
@@ -1,13 +1,13 @@
 from locust import TaskSet, task, HttpLocust
 
 class ConverterTasks(TaskSet):
-   @task
-   def request_page(self):
-       self.client.get("/2019/10/31/texto-400kb")
+#    @task
+#    def request_page(self):
+#        self.client.get("/2019/10/31/texto-400kb")
 
-   #  @task
-   #  def request_page(self):
-   #     self.client.get("/2019/10/31/20kb-e-imagem")
+    @task
+    def request_page(self):
+       self.client.get("/2019/10/31/20kb-e-imagem")
 
 
 class ApiUser(HttpLocust):
